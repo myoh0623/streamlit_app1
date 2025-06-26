04d9b173fc3da24432ae26c47e1e65860f449b1f
ohminy <myoh0623@gmail.com>
2025-06-26 15:57:59 +0900

    gender scores

--- pages/lab1.py
+++ pages/lab1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("lab1")
-url = "https://github.com/myoh0623/dataset/blob/main/student-mat.csv"
+url = "https://raw.githubusercontent.com/myoh0623/dataset/refs/heads/main/student-mat.csv"
 df = pd.read_csv(url)
 
 # 성별 평균 성적 비교(G3 컬럼은 성적을 나타낸다)
@@ -10,3 +10,8 @@ st.subheader("1. 성별에 따른 평균 최종 성적 (G3 컬럼)")
 gender_score = df.groupby("sex")["G3"].mean()
 
 st.bar_chart(gender_score)
+
+
+# 공부 시간별 평균 성적 을 bar chart로 studytime 컬럼으로 groupby 
+
+# walc(주말 알콜 섭취) 에 따른 평균 성적 을 bar chart로
